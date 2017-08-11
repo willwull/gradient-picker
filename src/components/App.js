286ff40bdb1e36bb8d0e128c13ba33eb116a531b
Willy Liu <arkazain@gmail.com>
2017-08-11 20:34:51 +0200

    🚧 Add basic stuff in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "../stylesheets/App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      angle: 90,
+      gradient: {
+        from: "#FF9A9E",
+        to: "#FAD0C4",
+      },
+    }
+  }
+
   render() {
+    const angle = this.state.angle;
+    const from = this.state.gradient.from;
+    const to = this.state.gradient.to;
+    const containerStyle = {
+      background: `linear-gradient(${angle}deg, ${from}, ${to})`,
+    }
     return (
-      <div className="App">
+      <div id="app-container" style={containerStyle}>
 
       </div>
     );
