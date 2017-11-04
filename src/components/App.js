8259c4223f16b4e740b641ba820f6f98fc668d28
Willy Liu <arkazain@gmail.com>
2017-11-04 15:54:56 +0100

    📱 Improve mobile layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Color from "color";
 import Card from "./Card";
 import "../stylesheets/App.css";
+import "../stylesheets/Responsive.css";
 
 class App extends React.Component {
   constructor(props) {
